207ef65b5b839312a099d72129d249be88bbdc7d
Lisa Bernhardt <honeylizard@users.noreply.github.com>
2017-07-10 17:24:38 -0400

    Refactor Social Media Widget

--- lib/classes/Social_Media_Widget.php
+++ lib/classes/Social_Media_Widget.php
@@ -10,6 +10,11 @@ class Social_Media_Widget extends WP_Widget {
 	private $admin_view_template = 'admin/widgets/social-media';
 
 	/**
+	 * @var string  $admin_item_view_template     The path to the admin view template for the widget URL item.
+	 */
+	private $admin_item_view_template = 'admin/widgets/social-media/item';
+
+	/**
 	 * @var string  $item_view_template     The path to the list view template for the widget.
 	 */
 	private $list_view_template = 'widgets/social-media/list';
@@ -23,6 +28,54 @@ class Social_Media_Widget extends WP_Widget {
 	 * @var string  $icon_template_path     The path to the social media icon set for the widget.
 	 */
 	private $icon_template_path = '';
+
+	/**
+	 * @var array   $social_media_icon_set  The set of social media icon files and the name to display.
+	 */
+	private $social_media_icon_set = [
+		'facebook' => [
+			'name' => 'Facebook',
+			'icon' => 'facebook-dreamstale25.png',
+		],
+		'twitter' => [
+			'name' => 'Twitter',
+			'icon' => 'twitter-dreamstale71.png',
+		],
+		'pinterest' => [
+			'name' => 'Pinterest',
+			'icon' => 'pinterest-dreamstale57.png',
+		],
+		'linkedin' => [
+			'name' => 'LinkedIn',
+			'icon' => 'linkedin-dreamstale45.png',
+		],
+		'youtube' => [
+			'name' => 'Youtube',
+			'icon' => 'youtube-dreamstale86.png',
+		],
+		'github' => [
+			'name' => 'GitHub',
+			'icon' => 'github2-dreamstale35.png',
+		],
+		'tumblr' => [
+			'name' => 'Tumblr',
+			'icon' => 'tumblr-dreamstale70.png',
+		],
+	];
+
+	/**
+	 * @var array   $default_instance     The default set of fields for the widget.
+	 */
+	private $default_instance = [
+		'title' => '',
+		'url_facebook' => '',
+		'url_twitter' => '',
+		'url_pinterest' => '',
+		'url_linkedin' => '',
+		'url_youtube' => '',
+		'url_github' => '',
+		'url_tumblr' => '',
+	];
 
 	/**
 	 * Register widget with WordPress.
@@ -53,107 +106,30 @@ class Social_Media_Widget extends WP_Widget {
 
 		$list = '';
 
-		// if the Facebook URL is set
-		$url_facebook = $instance['url_facebook'];
+		foreach ( $instance as $key => $value ) {
+			if ( $key != 'title' ) {
 
-		if ( $url_facebook ) {
-			$item_variables = [
-				'url' => $url_facebook,
-				'name' => 'Facebook',
-				'icon' => $this->icon_template_path . 'facebook-dreamstale25.png',
-			];
+				$social_media_site_name = substr($key, 4); // get the name after the url_ prefix
 
-			$view = new View($this->item_view_template, $item_variables);
-			$list .= $view->render();
-		}
+				// if the social media URL is set, add the icon view.
+				if ( isset($this->social_media_icon_set[$social_media_site_name]) ) {
 
-		// if the Twitter URL is set
-		$url_twitter = $instance['url_twitter'];
+					$name = $this->social_media_icon_set[$social_media_site_name]['name'];
+					$icon = $this->social_media_icon_set[$social_media_site_name]['icon'];
 
-		if ( $url_twitter ) {
-			$item_variables = [
-				'url' => $url_twitter,
-				'name' => 'Twitter',
-				'icon' => $this->icon_template_path . 'twitter-dreamstale71.png',
-			];
+					if ( $name && $icon ) {
+						$list .= $this->addSocialItemView($instance[$key], $name, $icon);
+					}
 
-			$view = new View($this->item_view_template, $item_variables);
-			$list .= $view->render();
-		}
-
-		// if the Pinterest URL is set
-		$url_pinterest = $instance['url_pinterest'];
-
-		if ( $url_pinterest ) {
-			$item_variables = [
-				'url' => $url_pinterest,
-				'name' => 'Pinterest',
-				'icon' => $this->icon_template_path . 'pinterest-dreamstale57.png',
-			];
-
-			$view = new View($this->item_view_template, $item_variables);
-			$list .= $view->render();
-		}
-
-		// if the LinkedIn URL is set
-		$url_linkedin = $instance['url_linkedin'];
-
-		if ( $url_linkedin ) {
-			$item_variables = [
-				'url' => $url_linkedin,
-				'name' => 'LinkedIn',
-				'icon' => $this->icon_template_path . 'linkedin-dreamstale45.png',
-			];
-
-			$view = new View($this->item_view_template, $item_variables);
-			$list .= $view->render();
-		}
-
-		// if the Youtube URL is set
-		$url_youtube = $instance['url_youtube'];
-		if ( $url_youtube ) {
-			$item_variables = [
-				'url' => $url_youtube,
-				'name' => 'Youtube',
-				'icon' => $this->icon_template_path . 'youtube-dreamstale86.png',
-			];
-
-			$view = new View($this->item_view_template, $item_variables);
-			$list .= $view->render();
-		}
-
-		// if the GitHub URL is set
-		$url_github = $instance['url_github'];
-		if ( $url_github ) {
-			$item_variables = [
-				'url' => $url_github,
-				'name' => 'GitHub',
-				'icon' => $this->icon_template_path . 'github2-dreamstale35.png',
-			];
-
-			$view = new View($this->item_view_template, $item_variables);
-			$list .= $view->render();
-		}
-
-		// if the Tumblr URL is set
-		$url_tumblr = $instance['url_tumblr'];
-
-		if ( $url_tumblr ) {
-			$item_variables = [
-				'url' => $url_tumblr,
-				'name' => 'Tumblr',
-				'icon' => $this->icon_template_path . 'tumblr-dreamstale70.png',
-			];
-
-			$view = new View($this->item_view_template, $item_variables);
-			$list .= $view->render();
+				}
+			}
 		}
 
 		$description = '';
-
 		if ( ! is_front_page() ) {
 			$description = __('Connect with me on social media.', 'honeylizard');
 		}
+
 		$view_variables = [
 			'title' => $title,
 			'description' => $description,
@@ -173,40 +149,17 @@ class Social_Media_Widget extends WP_Widget {
 	 * @return array
 	 */
 	public function update($new_instance, $old_instance) {
-		$instance = [
-			'title' => '',
-			'url_facebook' => '',
-			'url_twitter' => '',
-			'url_pinterest' => '',
-			'url_linkedin' => '',
-			'url_youtube' => '',
-			'url_github' => '',
-			'url_tumblr' => '',
-		];
+		$instance = $this->default_instance;
 
 		if ( ! empty($new_instance['title']) ) {
 			$instance['title'] = filter_var($new_instance['title'], FILTER_SANITIZE_STRING);
 		}
-		if ( ! empty($new_instance['url-facebook']) ) {
-			$instance['url_facebook'] = filter_var($new_instance['url-facebook'], FILTER_SANITIZE_URL);
-		}
-		if ( ! empty($new_instance['url-twitter']) ) {
-			$instance['url_twitter'] = filter_var($new_instance['url-twitter'], FILTER_SANITIZE_URL);
-		}
-		if ( ! empty($new_instance['url-pinterest']) ) {
-			$instance['url_pinterest'] = filter_var($new_instance['url-pinterest'], FILTER_SANITIZE_URL);
-		}
-		if ( ! empty($new_instance['url-linkedin'] ) ) {
-			$instance['url_linkedin'] = filter_var($new_instance['url-linkedin'], FILTER_SANITIZE_URL);
-		}
-		if ( ! empty($new_instance['url-youtube'] ) ) {
-			$instance['url_youtube'] = filter_var($new_instance['url-youtube'], FILTER_SANITIZE_URL);
-		}
-		if ( ! empty($new_instance['url-github'] ) ) {
-			$instance['url_github'] = filter_var($new_instance['url-github'], FILTER_SANITIZE_URL);
-		}
-		if ( ! empty($new_instance['url-tumblr'] ) ) {
-			$instance['url_tumblr'] = filter_var($new_instance['url-tumblr'], FILTER_SANITIZE_URL);
+
+		foreach ( $instance as $key => $value ) {
+			if ( $key != 'title' ) {
+				$social_media_site_name = substr($key, 4); // get the name after the url_ prefix
+				$instance[$key] = $this->sanitizeURL($new_instance['url-' . $social_media_site_name]);
+			}
 		}
 
 		return $instance;
@@ -221,16 +174,32 @@ class Social_Media_Widget extends WP_Widget {
 	 */
 	public function form($instance) {
 		if ( empty($instance) ) {
-			$instance = [
-				'title' => '',
-				'url_facebook' => '',
-				'url_twitter' => '',
-				'url_pinterest' => '',
-				'url_linkedin' => '',
-				'url_youtube' => '',
-				'url_github' => '',
-				'url_tumblr' => '',
-			];
+			$instance = $this->default_instance;
+		}
+
+		$text_fields = '';
+
+		foreach ( $instance as $key => $value ) {
+			if ( $key != 'title' ) {
+				$social_media_site_name = substr($key, 4); // get the name after the url_ prefix
+
+				if ( isset($this->social_media_icon_set[$social_media_site_name]) ) {
+					$name = $this->social_media_icon_set[ $social_media_site_name ]['name'];
+
+					if ( $name ) {
+						$field_variables = [
+							'id'    => $this->get_field_id($social_media_site_name),
+							'label' => $name . ' ' . __('URL', 'honeylizard'),
+							'name'  => $this->get_field_name('url-' . $social_media_site_name),
+							'value' => esc_attr($value),
+						];
+
+						$view = new View($this->admin_item_view_template, $field_variables);
+						$text_fields .= $view->render();
+					}
+				}
+
+			}
 		}
 
 		$view_variables = [
@@ -241,46 +210,59 @@ class Social_Media_Widget extends WP_Widget {
 
 			'instructions' => __('Enter the URL for the social media account to include it in the list. If the URL is empty, the icon will not appear in the list.', 'honeylizard'),
 
-			'facebook_id' => $this->get_field_id('facebook'),
-			'facebook_name' => $this->get_field_name('url-facebook'),
-			'facebook_label' => __('Facebook URL', 'honeylizard'),
-			'facebook_value' => esc_attr($instance['url_facebook']),
-
-			'twitter_id' => $this->get_field_id('twitter'),
-			'twitter_name' => $this->get_field_name('url-twitter'),
-			'twitter_label' => __('Twitter URL', 'honeylizard'),
-			'twitter_value' => esc_attr($instance['url_twitter']),
-
-			'pinterest_id' => $this->get_field_id('pinterest'),
-			'pinterest_name' => $this->get_field_name('url-pinterest'),
-			'pinterest_label' => __('Pinterest URL', 'honeylizard'),
-			'pinterest_value' => esc_attr($instance['url_pinterest']),
-
-			'linkedin_id' => $this->get_field_id('linkedin'),
-			'linkedin_name' => $this->get_field_name('url-linkedin'),
-			'linkedin_label' => __('Linkedin URL', 'honeylizard'),
-			'linkedin_value' => esc_attr($instance['url_linkedin']),
-
-			'youtube_id' => $this->get_field_id('youtube'),
-			'youtube_name' => $this->get_field_name('url-youtube'),
-			'youtube_label' => __('Youtube URL', 'honeylizard'),
-			'youtube_value' => esc_attr($instance['url_youtube']),
-
-			'github_id' => $this->get_field_id('github'),
-			'github_name' => $this->get_field_name('url-github'),
-			'github_label' => __('Github URL', 'honeylizard'),
-			'github_value' => esc_attr($instance['url_github']),
-
-			'tumblr_id' => $this->get_field_id('tumblr'),
-			'tumblr_name' => $this->get_field_name('url-tumblr'),
-			'tumblr_label' => __('Tumblr URL', 'honeylizard'),
-			'tumblr_value' => esc_attr($instance['url_tumblr']),
+			'text_fields' => $text_fields,
 		];
 
 		$view = new View($this->admin_view_template, $view_variables);
 		echo $view->render();
 
 		return ''; // Setting to 'noform' will stop the save button from being included.
+	}
+
+	/**
+	 * Cleans up the URL by removing all characters except letters, digits and a specific set of special characters.
+	 *
+	 * @link http://php.net/manual/en/filter.filters.sanitize.php
+	 *
+	 * @param string $url   The URL to sanitize.
+	 *
+	 * @return string
+	 */
+	private function sanitizeURL($url) {
+		$sanitized_url = $url;
+
+		if ( ! empty($url) ) {
+			$sanitized_url = filter_var($url, FILTER_SANITIZE_URL);
+		}
+
+		return $sanitized_url;
+	}
+
+	/**
+	 * Generates an HTML view of the social media link.
+	 * Returns an empty string if there is no social media link.
+	 *
+	 * @param string $url   The url for the social media site.
+	 * @param string $name  The name of the social media site.
+	 * @param string $icon  The filename of the social media icon image.
+	 *
+	 * @return string
+	 */
+	private function addSocialItemView($url, $name, $icon) {
+		$html = '';
+
+		if ( $url ) {
+			$item_variables = [
+				'url'  => $url,
+				'name' => $name,
+				'icon' => $this->icon_template_path . $icon,
+			];
+
+			$view = new View($this->item_view_template, $item_variables);
+			$html .= $view->render();
+		}
+
+		return $html;
 	}
 
 }
